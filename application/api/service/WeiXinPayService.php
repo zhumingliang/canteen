017b6dadd7f3dadde06f256fdfdfe48335aee0f2
zhumingliang <353575156@qq.com>
2019-11-15 23:24:12 +0800

    修改接口：修复个人选菜已消费次数判断。

--- application/api/service/WeiXinPayService.php
+++ application/api/service/WeiXinPayService.php
@@ -3,12 +3,35 @@
 
 namespace app\api\service;
 
+use EasyWeChat\Factory;
+
 class WeiXinPayService
 {
 
+
+    public function getApp()
+    {
+        $config = [
+            // 必要配置
+            'app_id' => 'wx60311f2f47c86a3e',
+            'mch_id' => '1555725021',
+            'key' => '1234567890qwertyuiopasdfghjklzxc',   // API 密钥
+
+            // 如需使用敏感接口（如退款、发送红包等）需要配置 API 证书路径(登录商户平台下载 API 证书)
+            'cert_path' => 'path/to/your/cert.pem', // XXX: 绝对路径！！！！
+            'key_path' => 'path/to/your/key',      // XXX: 绝对路径！！！！
+
+            'notify_url' => 'http://canteen.tonglingok.com/api/v1/wallet/WXNotifyUrl',     // 你也可以在下单时单独设置来想覆盖它
+        ];
+
+        $app = Factory::payment($config);
+        return $app;
+    }
+
     public function getPayInfo($data)
     {
-        $app = app('wechat.payment');
+        // $app = app('wechat.payment');
+        $app = $this->getApp();
         $result = $app->order->unify([
             'body' => $data['body'],
             'out_trade_no' => $data['out_trade_no'],
