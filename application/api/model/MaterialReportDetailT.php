02c16101efd04b6689e662e11bec5a7ec24bb479
zhumingliang <353575156@qq.com>
2019-12-16 22:13:45 +0800

    修改材料下单报表

--- application/api/model/MaterialReportDetailT.php
+++ application/api/model/MaterialReportDetailT.php
@@ -20,7 +20,7 @@ class MaterialReportDetailT extends Model
     public static function orderRecords($page, $size, $report_id)
     {
         $list = self::where('report_id', $report_id)
-            ->field('id,ordering_date，material,dinner_id,dinner,order_count,update_count as material_count,update_price as material_price')
+            ->field('id,ordering_date,material,dinner_id,dinner,order_count,update_count as material_count,update_price as material_price')
             ->order('create_time desc')
             ->paginate($size, false, ['page' => $page]);
         return $list;
