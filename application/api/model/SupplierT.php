2ca2df37037a03970dfdcf7e907cc1d7e7b1a59b
zhumingliang <353575156@qq.com>
2019-12-23 16:55:24 +0800

    小卖部管理-报表导出

--- application/api/model/SupplierT.php
+++ application/api/model/SupplierT.php
@@ -11,7 +11,11 @@ class SupplierT extends Model
 {
     public static function companySuppliers($company_id)
     {
-        $suppliers = self::where('c_id', $company_id)
+        $suppliers = self::where(function ($query) use ($company_id) {
+            if (!empty($company_id)) {
+                $query->where('c_id', $company_id);
+            }
+        })
             ->where('state', CommonEnum::STATE_IS_OK)
             ->field('id,name')
             ->select();
