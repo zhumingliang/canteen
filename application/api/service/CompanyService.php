b787be8e60ff186bbf027f59191a079873edd18c
zhumingliang <353575156@qq.com>
2020-02-23 02:03:51 +0800

    生成企业二维码

--- application/api/service/CompanyService.php
+++ application/api/service/CompanyService.php
@@ -258,7 +258,7 @@ class CompanyService
 
     public function getOutQRCode($company_id)
     {
-        $company = CompanyT::where('id', $company_id)->where('sate', CommonEnum::STATE_IS_OK)->find();
+        $company = CompanyT::where('id', $company_id)->where('state', CommonEnum::STATE_IS_OK)->find();
         if (!$company_id) {
             throw  new AuthException(['msg' => '企业不存在']);
         }
