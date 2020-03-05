59633ff4ceed0dea1d80151baf74e471f7ca1418
zhumingliang <353575156@qq.com>
2020-03-05 13:04:15 +0800

    测试退款

--- application/api/model/ShopProductStatisticV.php
+++ application/api/model/ShopProductStatisticV.php
@@ -10,6 +10,7 @@ class ShopProductStatisticV extends Model
 {
     public static function saleMoney($supplier_id, $time_begin, $time_end)
     {
+        $time_end=addDay(1,$time_end);
         $money = self::where('supplier_id', $supplier_id)
             ->whereBetweenTime('create_time', $time_begin, $time_end)
             ->sum('sell_money');
