caaff5d2f1d17c7212025b31561e647ee64c48e6
zhumingliang <353575156@qq.com>
2020-05-18 17:31:43 +0800

    修复外来人员就餐限制

--- application/api/service/OrderService.php
+++ application/api/service/OrderService.php
@@ -189,11 +189,13 @@ class OrderService extends BaseService
             'openid' => $openid,
             'company_id' => $company_id,
             'u_id' => $u_id,
-            'order_num' => makeOrderNo(),
+            'order_num' =>time(),// makeOrderNo(),
             'money' => $money,
             'status' => 'paid_fail',
             'method_id' => PayEnum::PAY_METHOD_WX,
-            'order_id' => $order_id
+            'order_id' => $order_id,
+            'type' => 'canteen'
+
         ];
         $order = PayT::create($data);
         if (!$order) {
