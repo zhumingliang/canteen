85b053f845bf6192312eadafd39324f13ef0bd84
zhumingliang <353575156@qq.com>
2020-05-28 01:01:33 +0800

    修改短信发送机制

--- application/api/service/SendSMSService.php
+++ application/api/service/SendSMSService.php
@@ -19,16 +19,16 @@ class SendSMSService
 {
     public function sendCode($phone, $type)
     {
-     /*   $code = rand(10000, 99999);
-        $params = ['code' => $code];
-        $res = SendSms::instance()->send($phone, $params, $type);
-        $token = Request::header('token');
-        $key = "code:" . $token;
-        if (key_exists('Code', $res) && $res['Code'] == 'OK') {
-            Redis::instance()->set($key, $phone . '-' . $code, 120);
-            return true;
-        }*/
-       // $this->msgTask($phone, $params, $type, $key);
+        /*   $code = rand(10000, 99999);
+           $params = ['code' => $code];
+           $res = SendSms::instance()->send($phone, $params, $type);
+           $token = Request::header('token');
+           $key = "code:" . $token;
+           if (key_exists('Code', $res) && $res['Code'] == 'OK') {
+               Redis::instance()->set($key, $phone . '-' . $code, 120);
+               return true;
+           }*/
+        // $this->msgTask($phone, $params, $type, $key);
 
         $code = rand(10000, 99999);
         $params = ['code' => $code];
@@ -36,7 +36,8 @@ class SendSMSService
         $this->sendSms($phone, 'canteen_' . $type, $params);
         $token = Request::header('token');
         $redis = new Redis();
-        $redis->set($token, $phone . '-' . $code, 120);
+        $key = "code:" . $token;
+        $redis->set($key, $phone . '-' . $code, 120);
         return true;
     }
 
@@ -46,7 +47,7 @@ class SendSMSService
         $data = [
             'phone_number' => $phone_number,
             "type" => $type,
-            "sign"=>"canteen",
+            "sign" => "canteen",
             "params" => empty($params) ? ['create_time' => date('Y-m-d H:i:s')] : $params
         ];
         $res = Http::sendRequest($url, $data);
@@ -55,75 +56,4 @@ class SendSMSService
         }
 
     }
-    //短信队列
-    private function msgTask($phone, $params, $type, $key)
-    {
-        //php think queue:work --queue sendMsgQueue
-        $jobHandlerClassName = 'app\api\job\SendMsg';//负责处理队列任务的类
-        $jobQueueName = "sendMsgQueue";//队列名称
-        $jobData = [
-            'phone' => $phone,
-            'params' => $params,
-            'type' => $type,
-            'token' => $key
-        ];;//当前任务的业务数据
-        $isPushed = Queue::push($jobHandlerClassName, $jobData, $jobQueueName);
-        //将该任务推送到消息队列
-        if ($isPushed == false) {
-            throw new SaveException(['msg' => '发送短信失败']);
-        }
-
-    }
-
-    public function saveSend($phone, $params, $type, $token = '')
-    {
-        $data = [
-            'phone' => $phone,
-            'params' => $params,
-            'type' => $type,
-            'token' => $token,
-            'failCount' => 0
-        ];
-        Redis::instance()->lPush('canteen_send_message', json_encode($data));
-    }
-
-    public function sendHandel()
-    {
-        try {
-            $redis = Redis::instance();
-            for ($i = 0; $i < 10; $i++) {
-                $data = $redis->rPop('canteen_send_message');//从结尾处弹出一个值,超时时间为60s
-                $data_arr = json_decode($data, true);
-                if (empty($data_arr['phone'])) {
-                    continue;
-                }
-                $res = SendSms::instance()->send($data_arr['phone'], $data_arr['params'], $data_arr['type']);
-                $data = [
-                    'phone' => $data_arr['phone'],
-                    'params' => $data_arr['params'],
-                    'type' => $data_arr['type'],
-                    'failCount' => $data_arr['failCount'] + 1
-                ];
-                if (key_exists('Code', $res) && $res['Code'] == 'OK') {
-                    $redis->lPush('canteen_send_message_success', json_encode($data));
-                    if (!empty($data_arr['token'])) {
-                        $redis = new Redis();
-                        $redis->set($data_arr['token'], $data_arr['phone'] . '-' . $data_arr['params']['code'], 120);
-                    }
-                } else {
-                    if ($data_arr['failCount'] > 2) {
-                        $data['failMsg'] = json_encode($res);
-                        $redis->lPush('canteen_send_message_fail', json_encode($data));
-
-                    } else {
-                        $redis->lPush('canteen_send_message', json_encode($data));
-                    }
-                }
-                usleep(100000);//微秒，调用第三方接口，需要注意频率，
-            }
-        } catch (Exception $e) {
-            LogService::save('sendHandel：' . $e->getMessage());
-        }
-    }
-
 }
