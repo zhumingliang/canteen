a074c671ef1451bd413e8e46eea49220a1a7df86
zhumingliang <353575156@qq.com>
2020-06-04 23:04:43 +0800

    测试导入excel

--- config/queue.php
+++ config/queue.php
@@ -16,7 +16,7 @@ return [
     'host'       => '127.0.0.1',	    // redis 主机ip
     'port'       => 6379,			    // redis 端口
     'password'   => '',                 // redis 连接密码
-    'select'     => 0,				    // 使用哪一个 db，默认为 db0
+    'select'     => 1,				    // 使用哪一个 db，默认为 db0
     'timeout'    => 0,				    // redis连接的超时时间
     'persistent' => false,			    // 是否是长连接
 ];
