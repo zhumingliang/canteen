583a0993214ecc37752c5eb3a1455ffed6ae762d
zhumingliang <353575156@qq.com>
2020-06-05 15:58:58 +0800

    测试

--- application/api/job/UploadExcel.php
+++ application/api/job/UploadExcel.php
@@ -101,7 +101,7 @@ class UploadExcel
         $data = (new ExcelService())->importExcel($fileName);
         $dataList = (new WalletService())->prefixUploadData($company_id, $admin_id, $data);
         $cash = (new RechargeCashT())->saveAll($dataList);
-        $this->clearUploading($company_id, $admin_id, $data['type']);
+        //$this->clearUploading($company_id, $admin_id, $data['type']);
         if (!$cash) {
             return false;
         }
