6110a1f2d27dd9362a84552f8b46034d671e9a26
zhumingliang <353575156@qq.com>
2020-07-01 00:25:39 +0800

    跨域增加域名

--- application/api/behavior/CORS.php
+++ application/api/behavior/CORS.php
@@ -20,9 +20,18 @@ class CORS
 {
     public function appInit($params)
     {
+        $origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
 
+        $allowOrigin = array(
+            'https://cloudcanteen3.51canteen.com',
+            'http://cloudcanteen3.51canteen.cn',
+        );
+
+        if (in_array($origin, $allowOrigin)) {
+            header("Access-Control-Allow-Origin:" . $origin);
+        }
         //解决跨域
-        header('Access-Control-Allow-Origin: https://cloudcanteen3.51canteen.com, http://cloudcanteen3.51canteen.cn');
+        // header('Access-Control-Allow-Origin: ,');
         header("Access-Control-Allow-Headers: token,Origin, X-Requested-With, Content-Type, Accept");
         header('Access-Control-Allow-Methods: POST,GET');
         header('X-Content-Type-Options: nosniff');
