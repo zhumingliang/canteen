c3ed9a7a1ded9250b55bf4419dbc58c2c3e4e6f2
zhumingliang <353575156@qq.com>
2020-07-01 15:48:35 +0800

    测试

---
+++ config/setting.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'domain' => 'https://tonglingok.com/canteen',
+    'image' => 'https://tonglingok.com',
+    'qrcode_expire_in' => 10,
+    'shop_qrcode_expire_in' => 10,
+    'qrcode_url' => 'type=%s&code=%s',
+    'token_cms_expire_in' => 3600 * 24,
+    'token_mini_expire_in' => 3600 * 2,
+    'token_machine_expire_in' => 3600 * 24 * 7,
+    'token_official_expire_in' => 3600 * 24,
+];
