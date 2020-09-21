86d69dec01afbc2b6cf19b8dfdd95d9144a8636d
zhumingliang <353575156@qq.com>
2020-09-21 19:34:08 +0800

    订单查询获取子订单信息优化

--- application/api/model/OrderParentT.php
+++ application/api/model/OrderParentT.php
@@ -70,7 +70,7 @@ class OrderParentT extends Model
                     $query->field('id,name,meal_time_end');
                 },
                 'sub' => function ($query) {
-                    $query->where('state', CommonEnum::STATE_IS_OK)->field('id,order_id,state,used,state,money,sub_money,used,order_sort,wx_confirm,sort_code,consumption_sort')
+                    $query->field('id,order_id,state,used,state,money,sub_money,used,order_sort,wx_confirm,sort_code,consumption_sort')
                         ->order('order_sort');
                 },
                 'foods' => function ($query) {
