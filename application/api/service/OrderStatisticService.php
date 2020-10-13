0d78fd21182466359e378678599e3e7c850e097f
zhumingliang <353575156@qq.com>
2020-10-14 03:32:27 +0800

    订餐明细导出修复

--- application/api/service/OrderStatisticService.php
+++ application/api/service/OrderStatisticService.php
@@ -112,7 +112,7 @@ class OrderStatisticService
                 $data['dinner'] = $v['dinner'];
                 $data['type'] = $v['type'];
                 $data['count'] = Num::numToWord($v['count']) . "份";
-                $data['money'] = $v['order_money'];
+                $data['money'] = $v['order_money'] / $v['count'];
                 $data['status'] = $this->getStatus($v['ordering_date'], $v['state'], $v['meal_time_end'], $v['used']);
                 $data['foods'] = $detail;
                 $data['all'] = $v['order_money'];
