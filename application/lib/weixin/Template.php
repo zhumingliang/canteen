217c0ed70c156447e09839fdc2ee7078fc3f8b2c
zhumingliang <353575156@qq.com>
2020-12-30 00:14:37 +0800

    测试

--- application/lib/weixin/Template.php
+++ application/lib/weixin/Template.php
@@ -4,11 +4,16 @@
 namespace app\lib\weixin;
 
 
+use app\api\service\LogService;
+
 class Template extends Base
 {
 
     public function send($openid, $template_id, $url, $data)
     {
+        LogService::saveJob($openid);
+        LogService::saveJob($template_id);
+        LogService::saveJob($url,json_encode($data));
         $res = $this->app->template_message->send([
             'touser' => $openid,
             'template_id' => $template_id,
