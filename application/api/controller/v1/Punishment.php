3bafd8e9843d453549a84a3a88778a0d702d962d
zhumingliang <353575156@qq.com>
2021-04-26 21:57:10 +0800

    惩罚策略更新

--- application/api/controller/v1/Punishment.php
+++ application/api/controller/v1/Punishment.php
@@ -18,7 +18,7 @@ class Punishment extends BaseController
      * @apiGroup  CMS
      * @apiVersion 3.0.0
      * @apiDescription  惩罚机制PC端-惩罚策略-获取测法策略列表
-     * @apiExample {get}  请求样例:
+     * http://canteen.tonglingok.com/api/v1/punishment/strategyDetail?page=1&size=10&&company_id=87&canteen_id=1
      * @apiParam (请求参数说明) {int} page 当前页码
      * @apiParam (请求参数说明) {int} size 每页多少条数据
      * @apiParam (请求参数说明) {String} company_id 企业id
@@ -33,12 +33,12 @@ class Punishment extends BaseController
      * @apiSuccess (返回参数说明) {int} company_id 企业id
      * @apiSuccess (返回参数说明) {int} canteen_id 饭堂id
      * @apiSuccess (返回参数说明) {int} staff_type_id 人员类型id
-     * @apiParam (请求参数说明) {string} detail  惩罚策略明细json字符串
-     * @apiSuccess (返回参数说明) {int} detail|id 惩罚策略明细表id
-     * @apiSuccess (返回参数说明) {int} detail|strategy_id 测法策略id
-     * @apiSuccess (返回参数说明) {string} detail|type 违规类型：no_meal 订餐未就餐；no_booking  未订餐就餐
-     * @apiSuccess (返回参数说明) {int}  detail|state 状态：1 正常；2 删除
-     * @apiSuccess (返回参数说明) {int}  detail|count 最大违规数量
+     * @apiSuccess (返回参数说明) {string} detail  惩罚策略明细json字符串
+     * @apiSuccess (返回参数说明) {int} id 惩罚策略明细表id
+     * @apiSuccess (返回参数说明) {int} strategy_id 测法策略id
+     * @apiSuccess (返回参数说明) {string} type 违规类型：no_meal 订餐未就餐；no_booking  未订餐就餐
+     * @apiSuccess (返回参数说明) {int}  state 状态：1 正常；2 删除
+     * @apiSuccess (返回参数说明) {int}  count 最大违规数量
      */
 
     public function strategyDetail($page = 1, $size = 10)
@@ -92,7 +92,7 @@ class Punishment extends BaseController
      * @apiParam (请求参数说明) {String} key 手机号或者姓名
      * @apiParam (请求参数说明) {int} company_id 企业id
      * @apiParam (请求参数说明) {string} company_name 企业关键字
-     * @apiParam (请求参数说明) {int} status 0传全部，状态 :1|正常;3|白名单;4|黑名单
+     * @apiParam (请求参数说明) {int} status 0传全部，状态 :1:正常;3:白名单;4:黑名单
      * @apiSuccessExample {json} 返回样例:
      * {"msg":"ok","errorCode":0,"code":200,"data":{"total":1,"per_page":"10","current_page":1,"last_page":1,"data":[{"company_id":87,"company_name":"小橙子","canteen_ids":"164","canteen_name":"饭堂","t_id":25,"staff_type":"测试","staff_id":6068,"username":"安全测试","phone":"18813960130","status":1,"no_meal":null,"no_booking":null}]}}
      * @apiSuccess (返回参数说明) {int} total 数据总数
