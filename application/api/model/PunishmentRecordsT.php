e0e58fc3170cfdfc977b68680c991107d3b6f708
zhumingliang <353575156@qq.com>
2021-05-03 00:31:52 +0800

    测试

--- application/api/model/PunishmentRecordsT.php
+++ application/api/model/PunishmentRecordsT.php
@@ -31,7 +31,7 @@ class PunishmentRecordsT extends Model
             ->leftJoin('canteen_canteen_t t3', 't3.id=t1.canteen_id')
             ->leftJoin('canteen_dinner_t t4', 't1.dinner_id =t4.id')
             ->leftJoin('canteen_company_department_t t5', 't5.id=t1.department_id')
-            ->where('company_id', $company_id)
+            ->where('t1.company_id', $company_id)
             ->field(['t1.day','t3.name as canteen_name','t5.name as department_name','t2.username','t4.name as meal_name','t1.type','t1.money']);
 
         return $punishStaff;
