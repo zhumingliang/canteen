29e55ed94af965e7eda4cd6479628fadd8b1b937
zhumingliang <353575156@qq.com>
2021-05-19 21:42:10 +0800

    测试

--- application/api/controller/v2/Token.php
+++ application/api/controller/v2/Token.php
@@ -37,6 +37,7 @@ class Token extends BaseController
     {
         $params = $this->request->param();
         $code = Request::param('code');
+        echo $code;
         if (empty($code)) {
             throw  new ParameterException(['msg' => "验证码不能为空"]);
         }
